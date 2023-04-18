17a415aafd22bfc48a3056521a905271ae5b1fd5
Sona Hasratyan <sonah@10web.io>
2023-04-19 03:35:52 +0400

    adding feature selection options via trees

--- preprocessor.py
+++ preprocessor.py
@@ -1,7 +1,13 @@
-# import numpy as np
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.inspection import permutation_importance
+
+
+# https://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html
+from sklearn.ensemble import RandomForestClassifier
 
 # import pickle
 
@@ -22,6 +28,7 @@ class Preprocessor:
         self.y_val = None
         self.features_to_drop = None
         self.scaler = None
+        self.features = None
         self.random_state = 78
 
     def fit(self):
@@ -44,6 +51,17 @@ class Preprocessor:
     def transform(self):
         self.X_train, self.y_train = self.__regularize_data(self.X_train, self.y_train)
         self.X_val, self.y_val = self.__regularize_data(self.X_val, self.y_val)
+
+        self.__feature_selection_mdi()
+        self.__feature_selection_permutation()
+        print(
+            set(self.features_mdi.keys()).intersection(
+                set(self.features_permutation.keys())
+            )
+        )
+
+        self.X_train, self.y_train = self.X_train.values, self.y_train.values
+        self.X_val, self.y_val = self.X_val.values, self.y_val.values
 
         self.__anomaly_detection(self.X_train)
 
@@ -80,9 +98,6 @@ class Preprocessor:
             f"Are there left any columns with nan values? - {any((X.isna().sum() * 100) / len(X) > 0)}"
         )
 
-        X = X.values
-        y = y.values
-
         return X, y
 
     def __set_features_to_drop(self, X):
@@ -93,7 +108,7 @@ class Preprocessor:
         columns_with_nans_statistics.sort_values("nans_percentage", inplace=True)
 
         print(
-            f'Number of columns including nans: '
+            f"Number of columns including nans: "
             f'{len(columns_with_nans_statistics[columns_with_nans_statistics["nans_percentage"] > 0])}'
         )
         # print(
@@ -113,6 +128,58 @@ class Preprocessor:
     def __anomaly_detection(self, X_train):
         # TODO
         pass
+
+    def __feature_selection_mdi(self):
+        rf = RandomForestClassifier(random_state=self.random_state)
+        rf.fit(self.X_train, self.y_train)
+
+        forest_importances = pd.Series(
+            rf.feature_importances_, index=self.X_train.columns
+        )
+        std = np.std([tree.feature_importances_ for tree in rf.estimators_], axis=0)
+
+        print(forest_importances.sort_values())
+        fig, ax = plt.subplots()
+        forest_importances[forest_importances <= 0.009].plot.bar(
+            yerr=std[forest_importances <= 0.009], ax=ax
+        )
+        ax.set_title("Feature importances using MDI")
+        ax.set_ylabel("Mean decrease in impurity")
+        fig.tight_layout()
+        fig.show()
+
+        # todo: make this features v
+        self.features_mdi = forest_importances[forest_importances > 0.009]
+        print(len(self.features_mdi))
+
+    def __feature_selection_permutation(self):
+        rf = RandomForestClassifier(random_state=self.random_state)
+        rf.fit(self.X_train, self.y_train)
+
+        permutation = permutation_importance(
+            rf,
+            self.X_val,
+            self.y_val,
+            n_repeats=10,
+            random_state=self.random_state,
+            n_jobs=2,
+        )
+        forest_importances = pd.Series(
+            permutation.importances_mean, index=self.X_val.columns
+        )
+        print(forest_importances.sort_values())
+        fig, ax = plt.subplots()
+        forest_importances[forest_importances <= 0.0].plot.bar(
+            yerr=permutation.importances_std[forest_importances <= 0.0], ax=ax
+        )
+        ax.set_title("Feature importances using MDI")
+        ax.set_ylabel("Mean decrease in impurity")
+        fig.tight_layout()
+        fig.show()
+
+        # todo: make this features v
+        self.features_permutation = forest_importances[forest_importances > 0.0]
+        print(len(self.features_permutation))
 
 
 preprocessor = Preprocessor()
